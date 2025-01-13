c8fffb8700964aecc1bb239b31f8d85b97555c1c
“Aman <aman@korecent.com>
2025-01-13 18:45:25 +0530

    changes commit

---
+++ assignone/assignment_first/doctype/items/items.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aman Kumar and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class items(Document):
+	pass
---
+++ assignone/assignment_first/doctype/store_manager/test_store_manager.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aman Kumar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Teststoremanager(FrappeTestCase):
+	pass
