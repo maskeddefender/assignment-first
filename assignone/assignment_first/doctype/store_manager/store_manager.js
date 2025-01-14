b7f98963adbe8c5033d88d76e20f44c7189d3b9b
“Aman <aman@korecent.com>
2025-01-14 10:58:53 +0530

    form chages in store manager

--- assignone/assignment_first/doctype/store_manager/store_manager.js
+++ assignone/assignment_first/doctype/store_manager/store_manager.js
@@ -1,16 +1,13 @@
 // Copyright (c) 2025, Aman Kumar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("store manager", {
-// 	refresh(frm) {
+frappe.ui.form.on("store manager", {
+	refresh(frm) {
 
-// 	},
-// });
+        frappe.msgprint("Hello User! Welcome to the Store Manager Page");
 
-if (!frm.doc.description) {
-    frm.set_intro('Please set the value of description', 'blue');
-}
-else {
-    frm.set_intro('Description is set', 'green');
-}
+	},
+});
+
+
 
