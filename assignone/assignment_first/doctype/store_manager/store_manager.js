6a445f8c16e9cf7b409b80c4b0cf74ff104cb238
“Aman <aman@korecent.com>
2025-01-14 11:23:29 +0530

    form chages in store manager

--- assignone/assignment_first/doctype/store_manager/store_manager.js
+++ assignone/assignment_first/doctype/store_manager/store_manager.js
@@ -9,5 +9,13 @@ frappe.ui.form.on("store manager", {
 	},
 });
 
-
+frappe.ui.form.on('livingroom', { // The child table is defined in a DoctType called "livingroom"
+    address_add(frm, cdt, cdn) { // "address" is the name of the table field in store manager, "_add" is the event
+        // frm: current store manager form
+        // cdt: child DocType 'livingroom'
+        // cdn: child docname (something like 'a6dfk76')
+        // cdt and cdn are useful for identifying which row triggered this event
+        frappe.msgprint('A row has been added to the livingroom table 🎉 ');
+    }
+});
 
