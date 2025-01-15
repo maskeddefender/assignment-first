eefdb32ca4881bafcc368c8b553e1d5b1c8323a6
“Aman <aman@korecent.com>
2025-01-15 10:03:09 +0530

    major changes

---
+++ assignone/assignment_first/doctype/cpin/cpin.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aman Kumar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("cpin", {
+// 	refresh(frm) {
+
+// 	},
+// });
--- assignone/assignment_first/doctype/phones/phones.js
+++ assignone/assignment_first/doctype/phones/phones.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2025, Aman Kumar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("phones", {
-// 	refresh(frm) {
+frappe.ui.form.on("phones", {
+    after_save: function (frm) {
+        
+        frappe.msgprint(__("You have successfully saved the phone number of '{0}'", 
+            [frm.doc.first_name + " " + frm.doc.last_name]));
+        
+        
+        if (!frm.doc.datetime) {            
+            frm.set_value('date_time', frappe.datetime.now_datetime());
+        }
 
-// 	},
-// });
+        
+        if (!frm.doc.last_name) {
+            frm.set_intro(__('Please set the value of description'), 'blue');
+        }
+    },
+
+    // Set 'full_name' by combining first and last name
+    refresh: function (frm) {
+        if (frm.doc.first_name && frm.doc.last_name) {
+            frm.set_value('full_name', `${frm.doc.first_name} ${frm.doc.last_name}`);
+        }
+    }
+});
